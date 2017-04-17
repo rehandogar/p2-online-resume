52224dc669de3f82973406db87210713b4327c7d
REHAN-PC\Arbisoft <rehanumardogar@gmail.com>
2017-04-17 20:11:10 +0500

    feat: bio section is added
    All objects are created in resumeBuilder.js and bio.display method is
    encapsulated inside the bio object. Which batch all possible reads in
    start of this method and all possible writes are also batched and added
    in the end of bio.display method.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,104 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+  "name": "Rehan Umar",
+  "role": "Front End Developer",
+  "contacts": {
+    "mobile": "0000-0000000",
+    "email": "rehanumardogar(at)gmail.com",
+    "github": "github.com/rehanumar",
+    "twitter": "twitter.com/RehanUmar8",
+    "location": "Azam Garden Main Bivd., Lahore, Pakistan"
+  },
+  "welcomeMessage": "Welcome here!",
+  "skills": [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "JQuery",
+    "AngularJS",
+    "And Many More!"
+  ],
+  "biopic":"images/fry.png"
+}
+
+var education = {
+  "schools": [
+    {
+      "name": "Superior College",
+      "location": "Lahore",
+      "degree": "F.sc Pre Engineering",
+      "majors": ["Physics","Chimstry","Math"],
+      "dates": "25 Dec, 2013",
+      "url": "https://www.superior.edu.pk/"
+    }
+  ],
+  "onlineCourses":[
+    {
+      "title": "Front End Nano Degree",
+      "school": "udacity",
+      "date": "15 Dec, 2016",
+      "url": "https://www.udacity.com/"
+    }
+  ]
+}
+
+var work = {
+  "jobs": [
+    {
+      "employer": "Arbisoft",
+      "title": "Front End Developer",
+      "location": "Lahore",
+      "dates": "25 Dec, 2013",
+      "description": "My job is to build the stunning front end experiences for users"
+    }
+  ]
+}
+
+var projects = {
+  "projects": [
+    {
+      "title": "Portfolio Site",
+      "dates": "03 Nov, 2016",
+      "description": "My personal portfolio site. Where I occasionally write blog posts.",
+      "images": [
+        "images/197x148.gif",
+        "images/197x148.gif"
+      ]
+    },
+    {
+      "title": "Classic Arcade Game",
+      "dates": "25 Dec, 2016",
+      "description": "A classic Arcade game (Frogger) which I have created using the canvas APIs",
+      "images": [
+        "images/197x148.gif",
+        "images/197x148.gif"
+      ]
+    }
+  ]
+}
+
+bio.display = function () {
+  var $header = $('#header');
+  var $topContacts = $('#topContacts');
+  var $footerContacts = $('#footerContacts');
+  var skillMarkup = '';
+  $header.prepend(HTMLheaderRole.replace('%data%', bio.role));
+  $header.prepend(HTMLheaderName.replace('%data%', bio.name));
+  $header.append(HTMLbioPic.replace('%data%', bio.biopic));
+  $header.append(HTMLwelcomeMsg.replace('%data%', bio.welcomeMessage));
+  $header.append(HTMLskillsStart);
+  $topContacts.append(HTMLmobile.replace('%data%', bio.contacts.mobile));
+  $topContacts.append(HTMLemail.replace('%data%', bio.contacts.email));
+  $topContacts.append(HTMLgithub.replace('%data%', bio.contacts.github));
+  $topContacts.append(HTMLtwitter.replace('%data%', bio.contacts.twitter));
+  $topContacts.append(HTMLlocation.replace('%data%', bio.contacts.location));
+  $footerContacts.append(HTMLmobile.replace('%data%', bio.contacts.mobile));
+  $footerContacts.append(HTMLemail.replace('%data%', bio.contacts.email));
+  $footerContacts.append(HTMLgithub.replace('%data%', bio.contacts.github));
+  $footerContacts.append(HTMLtwitter.replace('%data%', bio.contacts.twitter));
+  $footerContacts.append(HTMLlocation.replace('%data%', bio.contacts.location));
+  bio.skills.forEach(function (skill) {
+    skillMarkup = skillMarkup + HTMLskills.replace('%data%', skill);
+  });
+  $('#skills').append(skillMarkup);
+}
+bio.display();
